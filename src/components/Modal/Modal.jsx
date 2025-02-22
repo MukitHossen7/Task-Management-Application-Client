36543a6761f150ec4fd2d005126fdacbcdf0667c
MukitHossen7 <hossenmukit7@gmail.com>
2025-02-22 10:46:45 +0600

    Update Firebase hosting cache, enhance dependencies, and clean up unused code in components

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -27,7 +27,7 @@ const Modal = ({ isOpen, setIsOpen, refetch }) => {
   return (
     <div>
       {isOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30">
           <div className="bg-white p-6 rounded-lg shadow-lg w-96">
             <h2 className="text-xl font-semibold text-center mb-4">
               Add New Task
